73fe65d33a9cdda95cb99b38fa649db16865e350
hkx <hkx@192.168.0.101>
2020-12-05 15:14:11 +0800

    first commit

---
+++ src/firstProgram/HelloWorld.java
@@ -0,0 +1,16 @@
+package firstProgram;
+
+
+/**
+ * hello world
+ * @author hkx
+ * @date 2019-12-21
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello,world");
+	}
+
+}
---
+++ src/firstProgram/NineNine.java
@@ -0,0 +1,27 @@
+package firstProgram;
+
+/**
+ * ¾Å¾Å³Ë·¨±í
+ * @author hkx
+ * @date 2019-12-21
+ */
+public class NineNine {
+	
+	public static void main(String[] args) {
+/*		for(int i=0;i<9;i++) {
+			for (int j = 0; j <=i;j++) {
+				System.out.print("*");
+			}
+			System.out.print("\n");
+		}*/
+		
+		
+		for (int i = 1; i <= 9; i++) {
+			for (int j = 1; j <=i; j++) {
+				System.out.print(j+"x"+i+"="+j*i+"\t");
+			}
+			System.out.print("\n");
+		}
+	}
+	
+}
